6daea2b4cb77f529e46cac44e06421089116a260
vdaasdas <133831584+dikamaulanada@users.noreply.github.com>
2025-04-09 19:47:05 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,20 @@
-const CACHE_NAME = 'akademi-crypto-cache-v1';
-const assetsToCache = [
-  './',
-  './index.html',
-  './styles.css',
-  './script.js',
-  './manifest.json',
-  './images/logo.png',
-  './images/logo-192x192.png',
-  './images/logo-512x512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assetsToCache);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
-        .map((cacheName) => caches.delete(cacheName))
-      );
-    })
-  );
-});
+{
+  "name": "Akademi Crypto",
+  "short_name": "AkademiCrypto",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#121212",
+  "theme_color": "#4B0082",
+  "icons": [
+    {
+      "src": "images/logo-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "images/logo-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
